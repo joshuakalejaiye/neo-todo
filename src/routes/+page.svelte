62ac1343061d87c11e594dee37842f876f38bea7
Adedayo (Joshua) Kalejaiye <32837010+joshuakalejaiye@users.noreply.github.com>
2022-12-19 14:53:41 +0000

    remove m-l

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,30 +19,30 @@
 		});
 	};
 
-	$: completedTasks = todos.filter((todo) => todo.completed).length;
+	$: completedTasks = todos.filter(
+		(todo) => todo.completed
+	).length;
 	$: incompleteTasks = todos.length - completedTasks;
 	$: todos, console.table(todos);
 </script>
 
 <Hero {addTodos} />
 
-<div
-	class="mb-8"
->
+<div class="mb-8">
 	<!-- <div class="">
 		<div class="statsTitle">Stats</div>
 		<div><p>{`${incompleteTasks} Incomplete Tasks`}</p></div>
 		<div><p>{`${completedTasks} Tasks Completed`}</p></div>
 	</div> -->
 
-	<div class="mt-8 ml-9 flex flex-col items-center">
+	<div class="mt-8 flex flex-col items-center">
 		{#each todos as todo}
-		<TodoItem
-					text={todo.text}
-					completed={todo.completed}
-					onCheck={() => checkTodo(todo)}
-					onDelete={() => deleteTodo(todo)}
-				/>
+			<TodoItem
+				text={todo.text}
+				completed={todo.completed}
+				onCheck={() => checkTodo(todo)}
+				onDelete={() => deleteTodo(todo)}
+			/>
 		{/each}
-		</div>
+	</div>
 </div>
